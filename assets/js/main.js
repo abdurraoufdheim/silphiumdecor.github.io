31be64d64a33796cbb83fe753a29cd682e386add
Silphiumdecor <abdurraoufdheim@gmail.com>
2026-06-26 17:18:18 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
-// Silphium Decor JavaScript
+const toggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".nav-menu");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
